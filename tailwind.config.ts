fa31173a739147e924e0fa2de7aea8bf22f1b89a
I'm Cristian Andres Castro Rojas <147950986+CristianCastroRojas@users.noreply.github.com>
2024-11-03 22:38:14 -0500

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        secondary : "#ffd60a",
+        darkBg: "#131424"
       },
+      backgroundImage: {
+        "gradient-cover":
+          "linear-gradient(90.21deg, rgba(255, 255, 255, 0.5) -5.91%, rgba(0, 0, 0, 0.5) 111.58%)"
+      }
     },
   },
   plugins: [],
